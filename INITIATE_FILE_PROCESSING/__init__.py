54ec8089141bf2a2fe029ddd1f9c3bc422bf8adb
will <sopwill3@gmail.com>
2023-08-07 18:26:08 -0700

    INITIATE_FILE_PROCESSING is created; Added blob containers, queues, and Key Vault to Azure; write init_task_data util to create the initial json data for a new task; This funciton now works completeley and adds a task to the queue to trigger JSON extraction

--- INITIATE_FILE_PROCESSING/__init__.py
+++ INITIATE_FILE_PROCESSING/__init__.py
@@ -1,6 +1,15 @@
 import logging
+import json
+import uuid
 
 import azure.functions as func
+
+from azure.storage.blob import BlobClient
+from azure.storage.queue import QueueClient
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
+from utils.init_task_data import init_task_data
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -14,21 +23,82 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     Finally, this function returns the Task_ID back to the front-end.
     The front-end can use this idea to track progress with the CHECK_TASK_STATUS function
     '''
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('INITIATE_FILE_PROCESSING function hit')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    try:
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        # Fetch secrets and credentials
+        credential = DefaultAzureCredential()
+        blob_connection_str_secret = SecretClient(vault_url="https://files-to-qa-keys.vault.azure.net/", credential=credential).get_secret("BLOB-CONNECTION-STRING")
+        queue_connection_str_secret = SecretClient(vault_url="https://files-to-qa-keys.vault.azure.net/", credential=credential).get_secret("QUEUE-CONNECTION-STRING")
+
+    except Exception as e:
+        logging.error(f"Error during INITIATE_FILE_PROCESSING while getting Azure Credentials: {str(e)}")
+    
+        error_response = {
+            "status": "error",
+            "message": f"Failed to process the request. Error: {str(e)}"
+        }
+
+        return func.HttpResponse(json.dumps(error_response), status_code=500, mimetype="application/json")
+    
+    try:
+        logging.info(f"Received data: {req.form['data']}")
+
+        config_data = json.loads(req.form['data']) if 'data' in req.form else None
+
+
+        # Get data from request
+        file_data = req.files['file'].read() if 'file' in req.files else None
+        filename = req.files['file'].filename
+        file_size_in_bytes = len(file_data) if file_data else 0
+        
+        if not file_data:
+            return func.HttpResponse(json.dumps({"status": "error", "message": "Missing file in the request"}), status_code=400, mimetype="application/json")
+
+        if not config_data:
+            return func.HttpResponse(json.dumps({"status": "error", "message": "Missing metadata in the request"}), status_code=400, mimetype="application/json")
+
+    except Exception as e:
+        logging.error(f"Error during INITIATE_FILE_PROCESSING while getting reading file & config data: {str(e)}")
+    
+        error_response = {
+            "status": "error",
+            "message": f"Failed to process the request. Error: {str(e)}"
+        }
+
+        return func.HttpResponse(json.dumps(error_response), status_code=500, mimetype="application/json")
+
+    try:
+        # Create the unique IDs
+        task_id = str(uuid.uuid4())
+        task_id_meta = init_task_data(task_id, config_data, file_size_in_bytes, filename)
+        file_id = task_id_meta["raw_file_id"]
+
+        # Save the raw uploaded file to Blob storage
+        blob_client = BlobClient.from_connection_string(blob_connection_str_secret.value, container_name="raw-file-uploads", blob_name=file_id)
+        blob_client.upload_blob(file_data, overwrite=True)
+
+        # Create an empty task and store its status to Blob storage
+        task_status_blob_client = BlobClient.from_connection_string(blob_connection_str_secret.value, container_name="tasks-meta-data", blob_name=task_id)
+        task_status_blob_client.upload_blob(json.dumps(task_id_meta), overwrite=True)
+
+        # Add message to Queue to trigger next function
+        queue_client = QueueClient.from_connection_string(queue_connection_str_secret.value, queue_name="convert-to-json-queue")
+        queue_message = task_id_meta
+        queue_client.send_message(json.dumps(queue_message))
+
+        # Return task  data to frontend
+        return func.HttpResponse(json.dumps(task_id_meta), mimetype="application/json")
+    
+    # Add more granular exception handling (e.g., specific errors for blob storage failures, queue failures, etc.)
+    except Exception as e:
+        logging.error(f"Error during INITIATE_FILE_PROCESSING: {str(e)}")
+    
+        error_response = {
+            "status": "error",
+            "message": f"Failed to process the request. Error: {str(e)}",
+        }
+
+        return func.HttpResponse(json.dumps(error_response), status_code=500, mimetype="application/json")
+
---
+++ utils/init_task_data.py
@@ -0,0 +1,53 @@
+import datetime
+
+def init_task_data(task_id, config_data, file_size_in_bytes, filename):
+    '''
+    The goal of this function is to initialize the metadata for a new task (e.g. file-upload).
+    It will take in the task_id and create a JSON dictionary that will track the task across its lifecycle.
+    '''
+
+    task_id_meta_data = {
+        # Core Meta Data
+        "task_id": task_id,
+        "raw_file_id": f"{task_id}_raw",
+        "json_text_id": f"{task_id}_json",
+        "final_output_id": f"{task_id}_final",
+        "user_id": config_data.get("user_id"),
+        # Processing Config
+        "title": config_data.get("title"),
+        "tags": config_data.get("tags"),
+        "end_use": config_data["end_use"],
+        "answer_tone": config_data["answer_tone"],
+        "QA_examples": config_data["QA_examples"],
+        # Status Tracking
+        "status": "initiated",
+        "section_tracker": {},
+        "completion": "0%",
+        "date_created": str(datetime.datetime.now()),
+        "error_message": None,
+        "num_QA_pairs": 0,
+        "processing_time": 0,
+        "file_size_in_bytes": file_size_in_bytes,
+        "filename": filename
+    }
+    
+    return task_id_meta_data
+
+
+'''
+EXAMPLE CONFIG DATA JSON OBJECT:
+
+{
+    "user_id": "TESTUSER",
+    "title": "Statistics Randomization TEST",
+    "tags": ["stats", "report", "math"],
+    "end_use": "I want to make a Q&A model that knows all about statistics to help me in my job as a data scientist.",
+    "answer_tone": "It should be technical and smart. A critical thinker.",
+    "QA_examples":[
+      "Explain the dangers of Selecting on the Dependent Variable.\n\nIf you only look at instances when a phenomenon occured, you are trying to assess correlation without variation. You must have variations to make comparisons.",
+      "Why does causation not imply correlation?\n\nTake an example, fire-fighters are highly correlated with fire damage, but clearly they don't cause fires.",
+      "Why are counter-examples like 'I got the flu shot last year and I still got the flu' not a valid critique of average effects?\n\nBecasue the evidence is talking about average effects. For example, imagine there are three kinds of people: always sick, never sick, and vaccine responders. Even though the vaccine will improve odds for some, there are some who will still always get sick!"
+        ]
+}
+
+'''
